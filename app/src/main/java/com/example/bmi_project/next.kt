90d61933b83ffcafe1d06dfd9efeca55dce79f19
foizmahbub <foizmahbub01@gmail.com>
2024-11-04 09:46:18 +0600

    update

--- app/src/main/java/com/example/bmi_project/next.kt
+++ app/src/main/java/com/example/bmi_project/next.kt
@@ -20,7 +20,7 @@ class next : AppCompatActivity() {
 
             binding.BMITv.text =String.format("BMI is : %2f",bmi)
             binding.BMItypeTv.text=when(bmi){
-                in 1.00..18.4->"Under_weight"
+                in 1.00..18.5->"Under_weight"
                 in 18.5..24.9->"Perfect"
                 in 25.0..29.9->"Over Weight"
                 in 30.0..34.9->"OBESE"
