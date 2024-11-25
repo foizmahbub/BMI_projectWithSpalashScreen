38c9e2d7420641750ef18aeabd6129f263979432
foizmahbub <foizmahbub01@gmail.com>
2024-11-25 18:47:10 +0600

    update

--- app/src/main/java/com/example/bmi_project/next.kt
+++ app/src/main/java/com/example/bmi_project/next.kt
@@ -2,7 +2,9 @@ package com.example.bmi_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.bmi_project.databinding.ActivityNextBinding
+import java.lang.ArithmeticException
 
 class next : AppCompatActivity() {
     lateinit var binding: ActivityNextBinding
@@ -13,25 +15,50 @@ class next : AppCompatActivity() {
         binding.ShowBtn.setOnClickListener {
 
 
-            val height = binding.Edt1.text.toString().toDouble()/100
-            val weight =binding.Edt2.text.toString().toDouble()
-            val bmi=weight/(height*height)
+            val height = binding.Edt1.text.toString()
+            val weight = binding.Edt2.text.toString()
+            if (height.isEmpty() ) {
+                binding.BMITv.text = "please Enter Your Height. "
+                return@setOnClickListener
+
+            }else if (weight.isEmpty()) {
+                binding.BMITv.text = "please Enter Your  Weight. "
+                return@setOnClickListener
+            }
+
+            try {
+
+                val weight = weight.toDouble()
+                val height = height.toDouble()/100
+
+                if (weight <= 0 && height <= 0) {
+                    binding.BMITv.text = "Weight and height must be positive values."
+                    return@setOnClickListener
+
+                }
+                val bmi = weight / (height * height)
 
 
-            binding.BMITv.text =String.format("BMI is : %2f",bmi)
-            binding.BMItypeTv.text=when(bmi){
-                in 1.00..18.5->"Under_weight"
-                in 18.5..24.99->"Perfect"
-                in 25.0..29.99->"Over Weight"
-                in 30.0..34.99->"OBESE"
 
+            binding.BMITv.text = String.format("BMI is : %2f", bmi)
+            binding.BMItypeTv.text = when (bmi) {
+                in 1.00..18.5 -> "Under_weight"
+                in 18.5..24.99 -> "Perfect"
+                in 25.0..29.99 -> "Over Weight"
+                in 30.0..34.99 -> "OBESE"
 
 
                 else -> "Extremely OBESE"
             }
+            }catch (e:NullPointerException){
+                binding.BMITv.text="Invalid input!Please enter numeric values."
 
+            }catch (e:ArithmeticException){
+                binding.BMITv.text="Height must be greater then Zero."
+            }
         }
-
-
+        }
     }
-}
+
+
+
